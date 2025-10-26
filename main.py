d451b050e4edccad18ebb87a735cd8461120ed72
Joshua97-josh <joshuaofficial97@gmail.com>
2025-10-26 09:20:28 +0530

    done

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 orgins=["http://localhost:3000"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
